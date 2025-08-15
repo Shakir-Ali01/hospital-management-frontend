2ca972af9221d8639e8b671a72e77969c9055770
shakir-Ali01 <connectwithshakirali@gmail.com>
2025-08-15 16:03:39 +0530

    Create login and signUp page

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -8,27 +8,34 @@ const links=[
     {name:"Patients", url:"/patients", icon:<IconMoodHeart stroke={1.5}/>},
     {name:"Appointments", url:"/appointments", icon:<IconCalendarCheck stroke={1.5}/>},
     {name:"Pharmacy", url:"/pharmacy", icon:<IconVaccine stroke={1.5}/>},
+
 ]
 const Sidebar = () => {
   return (
-    <div className='w-64 bg-red-200 flex flex-col gap-7 items-center py-3'>
-        <div className='text-red-500 flex gap-1 items-center'>
+    <div className='flex'>
+        <div className='w-64'>
+
+        </div>
+    <div className='w-64 fixed h-screen hide-scrollbar overflow-y-auto bg-dark flex flex-col gap-7 items-center '>
+        <div className='fixed z-[500] py-3 bg-dark text-primary-400 flex gap-1 items-center'>
             <IconHeartbeat size={40} stroke={2.5}/>
             <span className='font-heading font-semibold text-3xl'>Pulse</span>
         </div>
+        {/* start menu(Navlink) of the sideBar icluding profile icon */}
+        <div className='flex flex-col gap-5 mt-20'>
         <div className='flex flex-col gap-1 items-center'>
-        <div className='bg-black p-1 rounded-full drop-shadow-xl'>
+        <div className='bg-primary-400  p-1 rounded-full drop-shadow-xl'>
              <Avatar src="avatar.avif" size="xl" alt="it's me" />
              
         </div>
-        <span className='font-medium'>Boss</span>
-        <Text c="dimmed" size="xs">Admin</Text>
+        <span className='font-medium text-light'>Boss</span>
+        <Text c="dimmed" size="xs" className='text-light'>Admin</Text>
         </div>
         <div className='flex flex-col gap-1'>
             {
                 links.map((link)=>{
-                    return <NavLink to={link.url} key={link.url} className={({isActive})=>`flex items-center gap-3 w-full font-medium text-neutral-900 px-5 py-5 rounded-lg 
-                              ${isActive?"bg-primary-400":"hover:bg-gray-100"}`}>
+                    return <NavLink to={link.url} key={link.url} className={({isActive})=>`flex items-center gap-3 w-full font-medium text-neutral-900 px-5 py-5 text-light rounded-lg 
+                              ${isActive?"bg-primary-400 text-dark":"hover:bg-gray-100 hover:text-dark"}`}>
                             {link.icon}
                             <span>{link.name}</span>
                        
@@ -36,6 +43,8 @@ const Sidebar = () => {
                 })
             }
         </div>
+    </div>
+    </div>
     </div>
   )
 }
